477d7d922dc6c90a776946350f7126e46f4bc79f
mdmrmmunna3 <mdmunnaking1209@gmail.com>
2024-12-14 11:35:57 +0600

    view products data

---
+++ procedul/showData.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>View table data</title>
+</head>
+<body>
+    <section>
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HTML Table Example</title>
+    <style>
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+        table, th, td {
+            border: 1px solid black;
+        }
+        th, td {
+            padding: 8px;
+            text-align: left;
+        }
+    </style>
+</head>
+<body>
+    <h2 style="text-align: center;">Product Details</h2>
+    <table>
+        <?php 
+        require_once('dbRootPath.php');
+
+        $pro_view = $dbConnect->query('select * from products_details');
+
+        echo "<tr>
+        <th>Procuct Name</th>
+        <th>Price</th>
+        <th>Brand Name</th>
+        <th>Contact</th>
+        </tr>
+        ";
+
+        if($pro_view->num_rows > 0) {
+            while(list($name, $contact, $pro_name, $pro_price) = $pro_view->fetch_row()) {
+                echo"
+                <tr>
+                <td>$pro_name</td>
+                <td>$pro_price</td>
+                <td>$name</td>
+                <td>$contact</td>
+            </tr>
+                
+                ";
+            }
+        }
+        
+       
+    
+   
+        ?>
+    </table>
+</body>
+</html>
+
+    </section>
+</body>
+</html>
